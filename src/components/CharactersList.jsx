107f9266480d28081579394a1b05928a8b998455
Vadim MARTIN <vadim.martin@etu.univ-poitiers.fr>
2024-11-18 17:46:49 +0100

    feature/sort-characters

--- src/components/CharactersList.jsx
+++ src/components/CharactersList.jsx
@@ -1,15 +1,45 @@
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { getCharacters } from "../api/characters-api";
 
-export function CharactersList({ characters = [] }) {
+export function CharactersList() {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const [sortBy, setSortBy] = useState(queryParams.get('sortBy') || 'name');
+  const [order, setOrder] = useState(queryParams.get('order') || 'asc');
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    setCharacters(getCharacters(sortBy, order));
+  }, [sortBy, order]);
+
   return (
-    <ul id="characters">
-      {characters.map((character) => (
-        <li key={character.id}>
-          <Link to={`/characters/${character.id}`}>
-            {character.name}
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <div>
+        <label>
+          Sort by:
+          <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+            <option value="name">Name</option>
+            <option value="modified">Modified</option>
+          </select>
+        </label>
+        <label>
+          Order:
+          <select value={order} onChange={(e) => setOrder(e.target.value)}>
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
+          </select>
+        </label>
+      </div>
+      <ul id="characters">
+        {characters.map((character) => (
+          <li key={character.id}>
+            <Link to={`/characters/${character.id}`}>
+              {character.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
